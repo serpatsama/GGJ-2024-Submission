a4657af21a32d9327ca8de2e2a07edad27ff3f08
serpatsama <keetarpalkuhs@gmail.com>
2024-01-27 02:51:37 +0530

    Sorry Daddy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool ismoving;
+    private float moveSpeed = 10.0f;
+    private Vector2 input;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!ismoving)
+        {
+            input.x = Input.GetAxis("Horizontal");
+            input.y = Input.GetAxis("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                Vector3 position = transform.position;
+                position.x += input.x;
+                position.y += input.y;
+                StartCoroutine(Move(position));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 destination)
+    {
+        ismoving = true;
+        while ((destination - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = destination;
+
+        ismoving = false;
     }
 }
